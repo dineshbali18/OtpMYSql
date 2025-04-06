3ae21c78af723be688a9b7dc9998bb37c14a0ed9
Dinesh Bali <dineshbali45@gmail.com>
2025-04-06 10:34:10 -0500

    added rds

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('otp_db', 'root', 'Gdpteam3', {
-  host: 'localhost',
+// const sequelize = new Sequelize('otp_db', 'root', 'Gdpteam3', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   logging: false,
+// });
+
+
+const sequelize = new Sequelize('otp_db', 'admin', 'Gdpteam3', {
+  host: 'database-1.cbm6q2wayzrp.us-east-2.rds.amazonaws.com',
   dialect: 'mysql',
   logging: false,
 });
-
 module.exports = sequelize;
